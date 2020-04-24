bad0713213139cbd0b7290163b8e2427ff44f331
anaamay <38352371+anaamay@users.noreply.github.com>
2020-04-24 12:53:16 +0530

    Add files via upload

---
+++ ril-fb-api-gateway/src/test/java/com/ril/fb/RilFbApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ril.fb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RilFbApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
